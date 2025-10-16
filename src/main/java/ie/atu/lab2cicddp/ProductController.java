71ff1e29bdcb498b1359a59ba22acb26780231cd
DAVIDPRENDERGAST-STU <g00405007@atu.ie>
2025-10-16 17:33:57 +0100

    added Feature addProduct

--- src/main/java/ie/atu/lab2cicddp/ProductController.java
+++ src/main/java/ie/atu/lab2cicddp/ProductController.java
@@ -1,12 +1,14 @@
 package ie.atu.lab2cicddp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/product")
 @RestController
 public class ProductController {
+    List<Product> myList = new ArrayList<>();
     @GetMapping("/hello")
     public String hello()
     {
@@ -16,6 +18,13 @@ public class ProductController {
     public Product getProduct()
     {
         Product myProduct = new Product("Tv", 499);
+        return myProduct;
+    }
+
+    @PostMapping("/addProduct")
+    public Product addProduct(@RequestBody Product myProduct)
+    {
+        myList.add(myProduct);
         return myProduct;
     }
 }
